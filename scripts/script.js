f1eedb55eb6ca316aa375bfc2648fa5bc1b0360f
Pranoy Dutta <pranoydll@gmail.com>
2016-09-20 11:45:07 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,19 @@ $(function(){
   $('#s').click(function(){
     $(this).text('SOCIAL TEXT');
   });
-  $('#s').click(function(){
-    $(this).text('SOCIAL TEXT');
+  $('#p').click(function(){
+    $(this).text('POLITICAL TEXT');
+  });
+  $('#r').click(function(){
+    $(this).text('RELIGOUS TEXT');
+  });
+  $('#i1').click(function(){
+    $(this).text('INTELLECTUAL TEXT');
+  });
+  $('#i2').click(function(){
+    $(this).text('INTERRELATIONAL TEXT');
+  });
+  $('#e').click(function(){
+    $(this).text('ECONOMIC TEXT');
   });
 });
