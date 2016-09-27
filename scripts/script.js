74484af546c3ae9ccbbef574d48211f051333972
Pranoy Dutta <pranoydll@gmail.com>
2016-09-27 00:02:05 -0500

    home commit Yay!

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,36 @@
 // Jquery goes here
 $(function(){
-  
+
   $('#s').click(function(){
     $('#main-text').text('Maglths have no race or gender and because of this, magliths have never suffered discrimination in their kingdom. Magliths are content with drilling underground to find the two things they need/want, gold and coal. Magliths are fueled by coal, and their main currency is gold.');
+    $('#s').css('width', 150+'px');
   });
   $('#p').click(function(){
     $('#main-text').text('POLITICAL TEXT');
+    $('#p').css('width', 150+'px');
   });
   $('#r').click(function(){
     $('#main-text').text('RELIGOUS TEXT');
+    $('#r').css('width', 150+'px');
   });
   $('#i1').click(function(){
     $('#main-text').text('INTELLECTUAL TEXT');
+    $('#i1').css('width', 150+'px');
   });
   $('#i2').click(function(){
     $('#main-text').text('INTERRELATIONAL TEXT');
+    $('#i2').css('width', 150+'px');
   });
   $('#t').click(function(){
     $('#main-text').text('TECH TEXT');
+    $('#t').css('width', 150+'px');
   });
   $('#e').click(function(){
     $('#main-text').text('ECONOMIC TEXT');
+    $('#e').css('width', 150+'px');
   });
   $('#home').click(function(){
     $('#main-text').text('HOME TEXT')
+    $('#home').css('width', 150+'px');
   });
 });
